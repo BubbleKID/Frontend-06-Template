46a87223e0309d6e39194bbc66c77515645c443e
Mark <fqchenxin@gmail.com>
2020-11-29 09:27:06 +1100

    step 2

--- Week 05/index.js
+++ Week 05/index.js
@@ -3,10 +3,20 @@ let object = {
   b: 2
 }
 
-let po = new Proxy(object, {
-  set(obj, prop, val) {
-    console.log(obj, prop, val);
-  }
-});
+let po = reactive(object);
+
+function reactive(object) {
+  return new Proxy(object, {
+    set(obj, prop, val) {
+      object
+      console.log(obj, prop, val);
+      return obj[prop];
+    },
+    get(obj, prop) {
+      console.error.log(obj, prop);
+      return obj[prop];
+    }
+  });
+}
 
 po.a = 5;
