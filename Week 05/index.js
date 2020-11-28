46700b8b0f9a18470d3a27f2dd12cdec61fa39b2
Mark <fqchenxin@gmail.com>
2020-11-29 10:22:55 +1100

    step 3

--- Week 05/index.js
+++ Week 05/index.js
@@ -1,3 +1,4 @@
+let callbacks = [];
 let object = {
   a: 1,
   b: 2
@@ -5,18 +6,28 @@ let object = {
 
 let po = reactive(object);
 
+effect(() => {
+  console.log(po.a);
+});
+
+function effect(callback) {
+  callbacks.push(callback);
+}
+
 function reactive(object) {
   return new Proxy(object, {
     set(obj, prop, val) {
-      object
-      console.log(obj, prop, val);
+      obj[prop] = val;
+      for(let callback of callbacks) {
+        callback();
+      }
       return obj[prop];
     },
     get(obj, prop) {
-      console.error.log(obj, prop);
+      console.log(obj, prop);
       return obj[prop];
     }
   });
 }
 
-po.a = 5;
+po.a = 30;
