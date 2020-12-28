b161adfe18628d6a49b6fc9d88640221fe2cfd43
Mark <fqchenxin@gmail.com>
2020-12-28 12:42:40 +1100

    step3

--- Week 09/parser.js
+++ Week 09/parser.js
@@ -1,7 +1,72 @@
 const EOF= Symbol('EOF'); // EOF: End Of File
 
 function data(c) {
+  if(c == '<') {
+    return tagOpen;
+  } else if (c == EOF) {
+    return;
+  } else {
+    return data;
+  }
+}
 
+function tagOpen(c) {
+  if(c == '/') {
+    return endTageOpen
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName(c);
+  } else {
+    return;
+  }
+}
+
+function endTageOpen(c) {
+  if(match(/^[a-zA-Z]$/)) {
+    return tagName(c);
+  } else if(c == '>') {
+
+  } else if (c == EOF) {
+
+  } else {
+
+  }
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName;
+  } else if (c == ">") {
+    return data;
+  } else {
+    return tagName;
+  }
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == ">") {
+    return data;
+  } else if (c == "=") {
+    return beforeAttributeName;
+  } else {
+    return attributeName(c);
+  }
+}
+
+function selfClosingStartTag(c) {
+    if (c == ">") {
+        currentToken.isSelfClosing = true;
+        return data;
+    } else if (c == "EOF") {
+
+    } else {
+
+    }
 }
 
 module.exports.parseHTML = function parseHTML(html) {
